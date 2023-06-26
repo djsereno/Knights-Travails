dd9a0fa80d62a9e330259358597842d2a2cd8084
djsereno <djsereno91@gmail.com>
2023-06-26 00:34:38 -0700

    Add styles and processing of step cells

--- knights-travails.js
+++ knights-travails.js
@@ -7,6 +7,7 @@ const Board = (boardSize = 8) => {
   const boardWrapper = document.createElement('div');
   let originElem;
   let targetElem;
+  let stepElems = [];
 
   const buildBoard = () => {
     boardWrapper.setAttribute('id', 'board');
@@ -43,7 +44,10 @@ const Board = (boardSize = 8) => {
   };
 
   const getCell = (cell) => {
-    return document.querySelector(`.cell[data-row="${cell[0]}"][data-col="${cell[1]}"]`);
+    let row, col;
+    if (typeof cell === 'object') [row, col] = cell;
+    if (typeof cell === 'string') [row, col] = cell.split(',').map((i) => +i);
+    return document.querySelector(`.cell[data-row="${row}"][data-col="${col}"]`);
   };
 
   const setCurrentCell = (cell) => {
@@ -58,9 +62,15 @@ const Board = (boardSize = 8) => {
     targetElem.classList.add('target');
   };
 
+  const setStepCells = (cells) => {
+    stepElems.forEach((cell) => cell.classList.remove('step'));
+    stepElems = cells.map(getCell);
+    stepElems.forEach((cell) => cell.classList.add('step'));
+  };
+
   buildBoard();
 
-  return { setCurrentCell, setTargetCell };
+  return { setCurrentCell, setTargetCell, setStepCells };
 };
 
 const Knight = (startRow = 0, startCol = 0) => {
@@ -86,30 +96,28 @@ const Knight = (startRow = 0, startCol = 0) => {
   };
 
   const knightMoves = (start = [row, col], end) => {
-    if (!checkValidMove(start) || !checkValidMove(end)) return null;
+    if (!checkValidMove(start) || !checkValidMove(end)) return [];
 
     const queue = [start];
     const visited = new Map();
     visited.set(start.toString(), 'ORIGIN');
-    // let i = 0;
     while (!visited.has(end.toString())) {
       let current = queue.shift();
       const moves = possibleMoves(current).filter((cell) => !visited.has(cell.toString()));
       moves.forEach((cell) => visited.set(cell.toString(), current.toString()));
       queue.push(...moves);
-      // i++;
-      // if (i > 50) return;
     }
 
-    // console.log(queue, visited);
-
-    let path = `[${end.toString()}]`;
+    let path = [end.toString()];
     let current = end.toString();
     while (current !== start.toString()) {
-      path = `[${visited.get(current)}] -> ${path}`;
+      path.unshift(visited.get(current));
       current = visited.get(current);
     }
-    console.log(path);
+    console.log(`You made it in ${path.length} moves!  Here's your path:`);
+    path.forEach((cell) => console.log(`[${cell}]`));
+
+    return path;
   };
 
   return { row, col, possibleMoves, currentCell, knightMoves };
@@ -124,5 +132,5 @@ const checkValidMove = (move) => {
 const board = Board(8);
 const knight = Knight(5, 5);
 board.setCurrentCell(knight.currentCell());
-
-knight.knightMoves([5, 5], [5, 4]);
+let moves = knight.knightMoves([5, 5], [5, 4]);
+board.setStepCells(moves);
