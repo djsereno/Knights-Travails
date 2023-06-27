27b6d6706cffd574e2432f6beddf4f023a4b8d98
djsereno <djsereno91@gmail.com>
2023-06-26 23:00:17 -0700

    Update display of step cells

--- knights-travails.js
+++ knights-travails.js
@@ -23,24 +23,11 @@ const Board = (boardSize = 8) => {
         cell.classList.add('cell');
         cell.setAttribute('data-row', row);
         cell.setAttribute('data-col', col);
-        cell.addEventListener('click', handleLeftClick);
-        cell.addEventListener('contextmenu', handleRightClick);
+        cell.addEventListener('click', handleClick);
+        cell.addEventListener('contextmenu', handleClick);
         boardWrapper.appendChild(cell);
       }
     }
-  };
-
-  const handleLeftClick = (event) => {
-    const row = event.currentTarget.getAttribute('data-row');
-    const col = event.currentTarget.getAttribute('data-col');
-    setCurrentCell([row, col]);
-  };
-
-  const handleRightClick = (event) => {
-    event.preventDefault();
-    const row = event.currentTarget.getAttribute('data-row');
-    const col = event.currentTarget.getAttribute('data-col');
-    setTargetCell([row, col]);
   };
 
   const getCell = (cell) => {
@@ -65,7 +52,10 @@ const Board = (boardSize = 8) => {
   const setStepCells = (cells) => {
     stepElems.forEach((cell) => cell.classList.remove('step'));
     stepElems = cells.map(getCell);
-    stepElems.forEach((cell) => cell.classList.add('step'));
+    stepElems.forEach((cell) => {
+      if (cell.classList.contains('current') || cell.classList.contains('target')) return;
+      cell.classList.add('step');
+    });
   };
 
   buildBoard();
@@ -73,9 +63,10 @@ const Board = (boardSize = 8) => {
   return { setCurrentCell, setTargetCell, setStepCells };
 };
 
-const Knight = (startRow = 0, startCol = 0) => {
-  const row = startRow;
-  const col = startCol;
+const Knight = () => {
+  let originRow, originCol;
+  let targetRow, targetCol;
+
   const movement = [
     [2, 1],
     [2, -1],
@@ -86,16 +77,26 @@ const Knight = (startRow = 0, startCol = 0) => {
     [-1, 2],
     [-1, -2],
   ];
-  const currentCell = () => [row, col];
 
-  const possibleMoves = (origin = [row, col]) => {
+  const setOrigin = (row, col) => ([originRow, originCol] = [row, col]);
+  const setTarget = (row, col) => ([targetRow, targetCol] = [row, col]);
+  const getOrigin = () => {
+    if (originRow === undefined || originCol === undefined) return null;
+    return [originRow, originCol];
+  };
+  const getTarget = () => {
+    if (targetRow === undefined || targetCol === undefined) return null;
+    return [targetRow, targetCol];
+  };
+
+  const possibleMoves = (origin = [originRow, originCol]) => {
     const moves = movement
       .map((move) => [origin[0] + move[0], origin[1] + move[1]])
       .filter((move) => checkValidMove(move));
     return moves;
   };
 
-  const knightMoves = (start = [row, col], end) => {
+  const knightMoves = (start = [originRow, originCol], end = [targetRow, targetCol]) => {
     if (!checkValidMove(start) || !checkValidMove(end)) return [];
 
     const queue = [start];
@@ -120,17 +121,34 @@ const Knight = (startRow = 0, startCol = 0) => {
     return path;
   };
 
-  return { row, col, possibleMoves, currentCell, knightMoves };
+  return { setOrigin, setTarget, getOrigin, getTarget, possibleMoves, knightMoves };
 };
 
-const checkValidMove = (move) => {
+function checkValidMove(move) {
   const row = move[0];
   const col = move[1];
   return row >= 0 && row < 8 && col >= 0 && col < 8;
-};
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  const row = +event.currentTarget.getAttribute('data-row');
+  const col = +event.currentTarget.getAttribute('data-col');
+
+  if (event.button === 0) {
+    board.setCurrentCell([row, col]);
+    knight.setOrigin(row, col);
+  } else if (event.button === 2) {
+    board.setTargetCell([row, col]);
+    knight.setTarget(row, col);
+  }
+
+  console.log(knight.getOrigin(), knight.getTarget());
+  if (knight.getOrigin() && knight.getTarget()) {
+    const moves = knight.knightMoves();
+    board.setStepCells(moves);
+  }
+}
 
 const board = Board(8);
 const knight = Knight(5, 5);
-board.setCurrentCell(knight.currentCell());
-let moves = knight.knightMoves([5, 5], [5, 4]);
-board.setStepCells(moves);
